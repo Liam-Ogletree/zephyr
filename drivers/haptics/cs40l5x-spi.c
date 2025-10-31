5ec7ce5ea587dc556d3904cffefa8f3eb4512e64
Liam Ogletree <liam.ogletree@cirrus.com>
2025-10-30 21:54:33 -0500

    drivers: haptics: Add support for CS40L5x haptic driver
    CS40L5x is a family (CS40L50/51/52/53) of haptics drivers designed
    for trackpads, tablets, and automotive applications.
    
    This PR provides basic functionality for ROM features and serves
    as a starting point for the upstream driver. Notably, loading RAM
    firmware, SPI,  and certain CS40L51/52 features (e.g., diagnostics,
    closed-loop algorithms) are excluded from this driver.
    
    Tested with internal/external boost configurations. Tested I2C.
    Tested all ROM features with each device variant (50/51/52/53),
    including ROM playback, "buzz" playback and configuration, real-time
    haptics playback and configuration, trigger playback and configuration,
    runtime haptics logging, and gain configuration.
    
    Signed-off-by: Liam Ogletree <liam.ogletree@cirrus.com>

---
+++ drivers/haptics/cs40l5x-spi.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025, Cirrus Logic, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief SPI Driver for Cirrus Logic CS40L5x Haptic Devices
+ */
+
+#include <zephyr/drivers/haptics/cs40l5x.h>
+#include "zephyr/logging/log.h"
+
+LOG_MODULE_REGISTER(CS40L5X_SPI, CONFIG_HAPTICS_LOG_LEVEL);
+
+#if CONFIG_HAPTICS_CS40L5X_SPI
+static bool cs40l5x_regmap_is_ready_spi(const struct device *const dev)
+{
+	const struct cs40l5x_config *config = dev->config;
+
+	return spi_is_ready_dt(&config->bus.spi);
+}
+
+static struct device *cs40l5x_regmap_get_device_spi(const struct device *const dev)
+{
+	const struct cs40l5x_config *const config = dev->config;
+
+	return (struct device *)&config->bus.spi.bus;
+}
+
+static int cs40l5x_regmap_read_spi(const struct device *const dev, const uint32_t addr,
+				   uint32_t *const rx, const uint32_t len)
+{
+	const struct cs40l5x_config *const config = dev->config;
+
+	LOG_INST_ERR(config->log, "SPI not currently supported (%d)", -EPERM);
+
+	return -EPERM;
+}
+
+static int cs40l5x_regmap_write_spi(const struct device *const dev, const uint32_t *const tx,
+				    const uint32_t len)
+{
+	const struct cs40l5x_config *const config = dev->config;
+
+	LOG_INST_ERR(config->log, "SPI not currently supported (%d)", -EPERM);
+
+	return -EPERM;
+}
+
+const struct cs40l5x_bus_io cs40l5x_bus_io_spi = {
+	.is_ready = cs40l5x_regmap_is_ready_spi,
+	.get_device = cs40l5x_regmap_get_device_spi,
+	.read = cs40l5x_regmap_read_spi,
+	.write = cs40l5x_regmap_write_spi,
+};
+
+#endif /* CONFIG_HAPTICS_CS40L5X_SPI */
